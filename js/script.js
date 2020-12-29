bcd6918f855d83f50ad22737d7c2def25e58a5fd
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2020-12-29 03:10:16 -0300

    Update 1.3.0 - Section 2 "News"
    Now on click in the section 2, she will goes an extend more 500px, and show more news about the game, in the final a  new link to move the costumer for website news

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.onload()
+
+function clicar(){
+    let newsContent = document.querySelector('.news_content')
+    let readMore = document.querySelector('#read_more')
+    let newsTittle = document.querySelector('#news_tittle')
+    newsTittle.style.marginBottom = "-10%"
+    readMore.style.bottom = "40%"
+    newsTittle.innerHTML = "News"
+    readMore.innerHTML = "Read more"
+    newsContent.style.height = "500px"
+    let test;
+    test = 1
+}
+function over(){
+    readMore.style.bottom ="20%"
+}
+function clicarF(){
+    
+    if(test == 1){
+        readMore.style.height = "230px"
+        test--
+    }
+}
