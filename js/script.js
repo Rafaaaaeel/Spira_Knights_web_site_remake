9516f9c8bb70ee9d9d80f9327f0b5f0a3e073841
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2020-12-31 03:38:52 -0300

    update 1.3.5 - Ajust section 4

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = function () {
     var newsTittle = document.querySelector('#news_tittle')
     var LineAjust = document.querySelector("#line_ajust")
     var arrow = document.querySelector(".arrow_box")
-
+    
     readMore.addEventListener('click', () =>{
         
         LineAjust.style.bottom = "-81px"
@@ -13,11 +13,9 @@ window.onload = function () {
         LineAjust.style.transition = '0.4s'
         newsTittle.innerHTML = "News"
         newsContent.style.height = "500px"
-        arrow.style.display = "initial"
+        arrow.style.display = "initial" 
         
     })
-
-
     arrow.addEventListener('click', () =>{
         
         newsTittle.style.marginBottom = "1.2%"
@@ -28,31 +26,15 @@ window.onload = function () {
         readMore.style.display = 'initial'
         arrow.style.display = "none"
     }) 
-
+    
     var mode_1 = document.querySelector('#mode1')
     var mode_2 = document.querySelector('#mode2')
     var mode_3 = document.querySelector('#mode3')
 
-    mode_1.addEventListener('mouseenter', ()=>{
-
-        mode_2.style.opacity = "30%"
-        mode_3.style.opacity = "30%"
-        mode_1.style.marginRight = "230px"
-        document.querySelector('.modes_box').style.marginLeft = "10%"
-        
-        
+    mode_2.addEventListener('click', ()=>{
+        mode_1.style.marginLeft ="4%"
+        mode_2.style.zIndex = '1'
+        mode_2.style.marginLeft ="8%"
     })
-
-    mode_1.addEventListener('mouseout', () =>{
-        mode_2.style.opacity = "100%"
-        mode_3.style.opacity = "100%"
-        mode_1.style.marginRight = "2px"
-        document.querySelector('.modes_box').style.marginLeft = "20%"
-    })
-
-    mode_2.addEventListener('mouseenter', () =>{
-        mode_2.style
-    })
-
 
 }
