1305c613347148ebe9e9d9c712d08f4bad887582
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2020-12-31 10:57:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,11 +30,37 @@ window.onload = function () {
     var mode_1 = document.querySelector('#mode1')
     var mode_2 = document.querySelector('#mode2')
     var mode_3 = document.querySelector('#mode3')
-
+    var position_x = 0;
     mode_2.addEventListener('click', ()=>{
         mode_1.style.marginLeft ="4%"
         mode_2.style.zIndex = '1'
         mode_2.style.marginLeft ="8%"
+        position_x++
+        console.log(position_x)
+        if(position_x > 1){
+            position_x = 1
+            mode_1.style.position = 'absolute'
+            mode_1.style.zIndex = '-1'
+
+        }
+    })
+    
+    mode_1.addEventListener('click', () =>{
+        if(position_x == 1){
+            mode_1.style.position = 'relative'
+            mode_2.style.marginLeft ="4%"
+            mode_1.style.zIndex = '1'
+            mode_1.style.marginLeft ="8%"
+            position_x++
+            console.log(position_x)
+        }
     })
 
+    mode_3.addEventListener('click', () => {
+        mode_3.style.zIndex = '1'
+        mode_3.style.marginLeft = "8%"
+        mode_1.style.marginLeft = '8%'
+        mode_3.style.zIndex = '1'
+        
+    })
 }
