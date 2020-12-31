fa14959f7450472276af97bc0ad97ea48569026b
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2020-12-30 21:47:45 -0300

    Update 1.3.4 - Ajust section 4
    Add 3 contents on section 4 "Modes"

--- js/script.js
+++ js/script.js
@@ -1,35 +1,58 @@
-    
-    
-    
-    
-    
-function clicar(){
-    let newsContent = document.querySelector('.news_content')
-    let readMore = document.querySelector('#read_more')
-    let newsTittle = document.querySelector('#news_tittle')
-    let LineAjust = document.querySelector("#line_ajust")
-    LineAjust.style.bottom = "-81px" //Ajust line 
-    newsTittle.style.marginBottom = "-10%"
-    readMore.style.display = "none"
-    LineAjust.style.transition = '0.4s'
-    newsTittle.innerHTML = "News"
-    newsContent.style.height = "500px"
-    let arrow = document.querySelector(".arrow_box")
-    arrow.style.display = "initial"
-    
-}
+window.onload = function () {
+    var newsContent = document.querySelector('.news_content')
+    var readMore = document.querySelector('#read_more')
+    var newsTittle = document.querySelector('#news_tittle')
+    var LineAjust = document.querySelector("#line_ajust")
+    var arrow = document.querySelector(".arrow_box")
+
+    readMore.addEventListener('click', () =>{
+        
+        LineAjust.style.bottom = "-81px"
+        newsTittle.style.marginBottom = "-10%"
+        readMore.style.display = "none"
+        LineAjust.style.transition = '0.4s'
+        newsTittle.innerHTML = "News"
+        newsContent.style.height = "500px"
+        arrow.style.display = "initial"
+        
+    })
 
 
-function close_news(){
-    let newsTittle = document.querySelector('#news_tittle')
-    newsTittle.style.marginBottom = "2%"
-    let newsContent = document.querySelector('.news_content')
-    newsContent.style.height = "230px"
-    let LineAjust = document.querySelector("#line_ajust")
-    LineAjust.style.bottom = "190px" //Ajust line
-    LineAjust.style.transition = '0.1s'
-    let readMore = document.querySelector('#read_more')
-    readMore.style.display = 'initial'
-    let arrow = document.querySelector(".arrow_box")
-    arrow.style.display = "none"
+    arrow.addEventListener('click', () =>{
+        
+        newsTittle.style.marginBottom = "1.2%"
+        newsTittle.innerHTML = "News -"
+        newsContent.style.height = "230px"
+        LineAjust.style.bottom = "190px" 
+        LineAjust.style.transition = '0.1s'
+        readMore.style.display = 'initial'
+        arrow.style.display = "none"
+    }) 
+
+    var mode_1 = document.querySelector('#mode1')
+    var mode_2 = document.querySelector('#mode2')
+    var mode_3 = document.querySelector('#mode3')
+
+    mode_1.addEventListener('mouseenter', ()=>{
+
+        mode_2.style.opacity = "30%"
+        mode_3.style.opacity = "30%"
+        mode_1.style.marginRight = "230px"
+        document.querySelector('.modes_box').style.marginLeft = "10%"
+        
+        
+    })
+
+    mode_1.addEventListener('mouseout', () =>{
+        mode_2.style.opacity = "100%"
+        mode_3.style.opacity = "100%"
+        mode_1.style.marginRight = "2px"
+        document.querySelector('.modes_box').style.marginLeft = "20%"
+    })
+
+    mode_2.addEventListener('mouseenter', () =>{
+        mode_2.style
+    })
+
+
 }
