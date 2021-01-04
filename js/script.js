92cad06a0096841db0b2a28f74cccaf788afbb49
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-01-03 23:47:42 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 window.onload = function () {
-    var newsContent = document.querySelector('.news_content')
-    var readMore = document.querySelector('#read_more')
-    var newsTittle = document.querySelector('#news_tittle')
-    var LineAjust = document.querySelector("#line_ajust")
-    var arrow = document.querySelector(".arrow_box")
+    var newsContent, readMore, newsTittle, LineAjust, arrow
+    newsContent = document.querySelector('.news_content')
+    readMore = document.querySelector('#read_more')
+    newsTittle = document.querySelector('#news_tittle')
+    LineAjust = document.querySelector("#line_ajust")
+    arrow = document.querySelector(".arrow_box")
     
     readMore.addEventListener('click', () =>{
         
@@ -27,40 +28,46 @@ window.onload = function () {
         arrow.style.display = "none"
     }) 
     
-    var mode_1 = document.querySelector('#mode1')
-    var mode_2 = document.querySelector('#mode2')
-    var mode_3 = document.querySelector('#mode3')
-    var position_x = 0;
+    var mode_1, mode_2, mode_3;
+
+    mode_1 = document.querySelector('#mode1')
+    mode_2 = document.querySelector('#mode2')
+    mode_3 = document.querySelector('#mode3')
+
+    position_x = 0;
     mode_2.addEventListener('click', ()=>{
         mode_1.style.marginLeft ="4%"
         mode_2.style.zIndex = '1'
         mode_2.style.marginLeft ="8%"
-        position_x++
-        console.log(position_x)
-        if(position_x > 1){
-            position_x = 1
-            mode_1.style.position = 'absolute'
-            mode_1.style.zIndex = '-1'
-
-        }
+        position_x = 1
+        document.querySelector('#mode_tittle').innerHTML = 'Lockdown'
+        document.querySelector('#mode_p').innerHTML = "All weapons are available in this head-to-head brawl, in which Knights are divided into two teams to score points by taking over capture points or attack opposing team players. However, the shield function is replaced by unique class modification abilities: Strikers get a speed bonus, Guardians an area shield, and Recons invisibility. Each class receives bonuses and penalties relating to their role. <br><br> The entry fee is 200 crowns, and players must wait in queue until there are both enough knights for the next match, and space in the available instances. If a player chooses to drop out of the queue, the entry fee will be refunded."
     })
     
     mode_1.addEventListener('click', () =>{
-        if(position_x == 1){
-            mode_1.style.position = 'relative'
-            mode_2.style.marginLeft ="4%"
-            mode_1.style.zIndex = '1'
-            mode_1.style.marginLeft ="8%"
-            position_x++
-            console.log(position_x)
-        }
+        mode_2.style.zIndex ='0'
+        mode_2.style.marginLeft ="4%"
+        mode_1.style.marginLeft ="8%"
+        position_x = 0
+        document.querySelector('#mode_tittle').innerHTML = 'Blast Netowk'
+        document.querySelector('#mode_p').innerHTML = "<strong>Blast Network </strong> is an explosive event in <a href='https://wiki.spiralknights.com/King_Krogmo%27s_Coliseum' target='_blank'> King Krogmo's Coliseum </a> where Knights try to eliminate their opponents by blowing them up. All of your personal equipment is ignored in Blast Network and you instead have use of a single weapon, a <a target='_blank' href='https://wiki.spiralknights.com/Blast_Network_Bomb'> bomb </a>that explodes in a cross formation. Bombs do not require charging and can be placed immediately. A bomb will kill any Knight caught in its explosion, including the Knight who placed it, so be careful! In each game you earn points for having your bombs eliminate your opponents. The player or team with the most points at the end of the match wins <br><br>The cumulative weekly scores for the leaderboard are reset on Sunday"
+        
+
     })
 
     mode_3.addEventListener('click', () => {
-        mode_3.style.zIndex = '1'
+        if(position_x == 1){
+            mode_2.style.zIndex = '-1'
+        }
+    document.querySelector('#mode_p').innerHTML = "<iframe width='560' height='315' src='https://www.youtube.com/embed/0dYfMPYhPvQ' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.querySelector('#btest').style.opacity = '100%'
+    document.querySelector('#modes_t').style.color = 'white'
+    mode_3.style.transform = '1.2'
+        mode_2.style.marginLeft ='8%'
         mode_3.style.marginLeft = "8%"
         mode_1.style.marginLeft = '8%'
         mode_3.style.zIndex = '1'
         
     })
+
 }
